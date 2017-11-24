9f9bfbf7df9f87044a670f1fff19647460164663
Raul Sanchez <raul@um.es>
2017-11-24 13:28:07 +0100

    qitada linea

--- library-monolith/src/main/java/es/codemotion/madrid/library/ServletInitializer.java
+++ library-monolith/src/main/java/es/codemotion/madrid/library/ServletInitializer.java
@@ -7,6 +7,5 @@ public class ServletInitializer extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(CodemotionLibraryApplication.class);
-        
     }
 }
